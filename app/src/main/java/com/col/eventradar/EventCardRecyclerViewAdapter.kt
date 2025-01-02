c090f883bb996071367becf93d959a01cdbe8daa
Lior Vainer <liorvainer@gmail.com>
2025-01-02 20:38:25 +0200

    feat(fragment_event_card_list): added fragment_event_card_list.xml and fragment_event_card.xml and the recycler view at MainActivity and added start dest in nav_graph.xml

---
+++ app/src/main/java/com/col/eventradar/EventCardRecyclerViewAdapter.kt
@@ -0,0 +1,50 @@
+package com.col.eventradar
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.col.eventradar.placeholder.PlaceholderContent.PlaceholderItem
+import com.col.eventradar.databinding.FragmentEventCardBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class EventCardRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<EventCardRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentEventCardBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentEventCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
