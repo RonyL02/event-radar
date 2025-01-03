9684715f6776a9d08a754ccf76e70fd54a0fd1d9
Lior Vainer <liorvainer@gmail.com>
2025-01-03 17:56:03 +0200

    fix(MainActivity): added bottom navbar to main activity and connected it as navController

--- app/src/main/java/com/col/eventradar/MainActivity.kt
+++ app/src/main/java/com/col/eventradar/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostController: NavHostFragment? =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment?
+        navController = navHostController?.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        navController?.let {
+            Navigation.setViewNavController(
+                bottomNavigationView,
+                it,
+            )
         }
     }
 }
