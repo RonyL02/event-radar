15426c34385da542493d99994219f27d40d01178
RonyL02 <280202rony@gmail.com>
2025-01-19 01:09:46 +0200

    add click listeners + make layout more reactive

--- app/src/main/java/com/col/eventradar/ui/bottom_sheets/EventDetailsBottomSheet.kt
+++ app/src/main/java/com/col/eventradar/ui/bottom_sheets/EventDetailsBottomSheet.kt
@@ -7,14 +7,50 @@ import android.view.ViewGroup
 import com.col.eventradar.databinding.FragmentEventDetailsBottmSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class EventDetailsBottomSheet : BottomSheetDialogFragment() {
+data class EventDetails(
+    val type: Int,
+    val name: String,
+    val locationName: String,
+    val time: String,
+    val description: String,
+    val commentsAmount: Int,
+)
+
+class EventDetailsBottomSheet(private val eventDetails: EventDetails) :
+    BottomSheetDialogFragment() {
+    private var bindingInternal: FragmentEventDetailsBottmSheetBinding? = null
+    private val binding get() = bindingInternal!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = FragmentEventDetailsBottmSheetBinding.inflate(inflater, container, false).root
+    ): View {
+        bindingInternal = FragmentEventDetailsBottmSheetBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            eventTitle.text = eventDetails.name
+            eventDescription.text = eventDetails.description
+            time.text = eventDetails.time
+            commentsCount.text = eventDetails.commentsAmount.toString()
+            closeButton.setOnClickListener {
+                dismiss()
+            }
+            addCommentButton.setOnClickListener {
+                val commentText = commentInput.text
+                //TODO: send commentText to db and image (if exists)
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingInternal = null
+    }
 
     companion object {
-        const val TAG = "ModalBottomSheet"
+        const val TAG = "EventDetailsModalBottomSheet"
     }
 }
