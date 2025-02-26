3c39423fbca4ec973d6cba65fc3a7eabbcb00589
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2025-02-26 22:33:21 +0200

    login view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.recyclerview)
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
