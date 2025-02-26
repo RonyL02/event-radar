894c6612e090449ce977f2cd765ae31da41f2645
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2025-02-26 13:11:39 +0200

    login view

--- app/src/main/java/com/col/eventradar/LoginActivity.kt
+++ app/src/main/java/com/col/eventradar/LoginActivity.kt
@@ -1,19 +1,26 @@
 package com.col.eventradar
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.col.eventradar.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "ResourceType")
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginn)) { v, insets ->
+
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.login) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
